11d4c58a00770d6974764804b28081dc80de64e4
unknown <hamaregaya20@gmail.com>
2026-02-15 10:50:32 +0100

    final 04

--- backend/middlewares/nodemailer.js
+++ backend/middlewares/nodemailer.js
@@ -5,7 +5,7 @@ export const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    user: "7516bb002@smtp-brevo.com",
-    pass: "xsmtpsib-055e33c9079a10589a0259c761b07a32b124b45497c3582e5c70b87680fef6c6-7dZYOSRZy359mR20",
+    user: process.env.NODEMAILER_USER,
+    pass: process.env.NODEMAILER_PASS,
   },
 });
